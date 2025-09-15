523f1611a6fa55b183762e0142109a967a2082df
brpuneet898 <brpuneet898@gmail.com>
2025-09-16 00:19:18 +0530

    added login pages

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/seekerlogin')
+def seeker_login():
+    return render_template('seekerlogin.html')
+
+@app.route('/providerlogin')
+def provider_login():
+    return render_template('providerlogin.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
